1111b9a3cc586abac195bd696a0f10bf99add96c
ozzi- <oz@oz-web.com>
2018-03-01 22:11:55 +0100

    Update index.php
    refactoring and comments

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
     include_once("helpers.php");
 	
-	$triggersFile="triggers.php";
-	$updateFile="update.zip";
-	$updateSignatureFile="signature";
-	$updateURL = "https://127.0.0.1/updatesrv/index.php";
-	$pubkeyPath = "public_key.pem";
+	$triggersFile="triggers.php"; # defines what filename will be considered for running the post-unzip installation step
+	$updateFile="update.zip"; # how the downloaded update shall be called
+	$updateSignatureFile="signature"; # how the temporary signature file shall be called
+	$updateURL = "https://127.0.0.1/updatesrv/index.php"; # the URL to the update server
+	$pubkeyPath = "public_key.pem"; # path to the public key
 
 	if(isset($_GET['step'])){
 		$step=$_GET['step'];
@@ -32,7 +32,7 @@
 				echo("<br>".$nv['version']." Release Notes:<br><i>".$nv['releasenotes']."</i><br><br>");
 				echo('<a href="index.php?step=download">Download</a>');
 			}else{
-				echo("<span class=\"oi oi-check\"></span> You are using the newest version.<br><br>Release notes:<br>");
+				echo("You are using the newest version.<br><br>Release notes:<br>");
 				echo("<i>".$version['releasenotes']."</i>");
 			}
 		}
@@ -51,17 +51,17 @@
 				$zip->close();
 				echo("Starting Update<br>");
 				if(file_exists($triggersFile)){
-					echo("<span class=\"oi oi-bolt\"></span>Running triggers<br><i>");
+					echo("Running triggers<br><i>");
 					include($triggersFile);
 					echo("</i><br>");
 				}
-				echo("<span class=\"oi oi-check\"></span> Installation complete.<br>");
+				echo("Installation complete.<br>");
 				echo("<br>");
 				?><form action="index.php?p=update" method="POST">
-					<button class="btn" style="background:<?= BRANDING_COLOR ?>">Continue</button>
+					<button>Continue</button>
 				</form> <?php
 			}else{
-				echo("Update failed (1) - ".$res."<br>");
+				echo("Update failed - ".$res."<br>");
 			}
 			$cu1=unlink($updateFile);
 			$cu2=unlink($updateSignatureFile);
@@ -78,14 +78,16 @@
 			}
 		}
 	}
+
 	function handleDownload($updateFile,$updateSignatureFile,$pubkeyPath){
 		$nv=newVersionAvailable();
 		$downloadOK = downloadAndCheckUpdate($updateFile,$updateSignatureFile,$nv['buildid'],$pubkeyPath);
 		if($downloadOK){
-			echo("<span class=\"oi oi-check\"></span> Downloaded newest version<br><span class=\"oi oi-check\"></span> Signature OK<br>");
+			echo("Downloaded newest version<br>");
+			echo("Signature OK<br>");
 			echo('<a href="index.php?step=install">Install</a>');
 		}else{
-			echo("<span class=\"oi oi-x\"></span> Downloaded newest version.<b> Signature INVALID</b>. Aborting");
+			echo("<b>Signature invalid or download corrupt</b> - Aborting");
 		}
 	}
   
