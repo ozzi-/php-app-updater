f71fb6e5706506ea1b71721d49e65c300af39059
ozzi- <oz@oz-web.com>
2018-03-01 18:23:21 +0100

    Add files via upload

---
+++ helpers.php
@@ -0,0 +1,87 @@
+ <?php
+	function updateVersionFile(){
+		global $updateURL;
+		$version = getCurrentVersion();
+		$newerURL = $updateURL."?operation=update&buildid=".$version["buildid"];
+		$ret = doGet($newerURL);
+		if(!$ret){
+		  echo("Failed updating version file");
+		}else{
+		  return file_put_contents("version",$ret[1]);
+		}
+	}
+
+	function getCurrentVersion(){
+		$version=json_decode(file_get_contents('version'),true);
+		if($version===NULL){
+		  echo("Failed loading version file");
+		  die();
+		}
+		return $version;
+	}
+
+	function newVersionAvailable(){
+		global $updateURL;
+		$version = getCurrentVersion();
+		$newerURL = $updateURL."?operation=update&buildid=".$version["buildid"];
+		$ret = doGet($newerURL);
+		if($ret[0]===200){
+			return json_decode($ret[1],true);
+		}
+		return false;
+	}
+
+	function downloadAndCheckUpdate($updateFile,$updateSignatureFile,$currentBuildID,$pubkeyPath){
+		global $updateURL;
+		$downloadURL = $updateURL."?operation=download&buildid=".$currentBuildID;
+
+		$retCode  = downloadFile($updateFile,$downloadURL);
+		if($retCode==200){
+			downloadFile($updateSignatureFile,$downloadURL."&signature");
+
+			$key = openssl_pkey_get_public(file_get_contents($pubkeyPath));
+			$hash=(hash_file("sha512",$updateFile));
+			$signature=base64_decode(file_get_contents($updateSignatureFile));
+
+			openssl_public_decrypt ( $signature , $decrypted , $key);
+			$decrypted = trim($decrypted);
+			return ($decrypted === $hash);
+		}else{
+			die("Received status code $retCode from update server. Expected 200. Aborting");
+		}
+	}
+
+	function doGet($url) {
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$content  = @curl_exec($ch);
+		if(curl_errno($ch)){
+			die("Network error. ".curl_error($ch));
+		}
+		$code = curl_getinfo($ch,CURLINFO_RESPONSE_CODE);
+		curl_close($ch);
+		return array($code,$content);
+		}
+
+		function downloadFile($name,$url){
+		$fp = fopen($name, 'w+');
+		if($fp === false){
+			throw new Exception('Could not open: ' . $saveTo);
+		}
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		@curl_exec($ch);
+		if(curl_errno($ch)){
+		  die("Downloading update failed. ".curl_error($ch));
+		}
+		$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+		return $statusCode;
+	}
+?>
---
+++ index.php
@@ -0,0 +1,114 @@
+<style>
+	.loader {
+		border: 16px solid <?= BRANDING_COLOR ?>;
+		border-top: 16px solid <?= BRANDING_WARNING_COLOR ?>;
+		border-radius: 50%;
+		width: 120px;
+		height: 120px;
+		animation: spin 2s linear infinite;
+	}
+	.centered {
+		position: fixed; 
+		top: 50%;
+		left: 50%;
+	}
+</style>
+<script>
+	function showSpinner(){
+	document.getElementById("spinnerbkp").style.display="block";
+	}
+</script>
+<div id="spinnerbkp" class="loader centered" style="display: none;"></div>
+
+<?php
+    include_once("helpers.php");
+	
+	$triggersFile="triggers.php";
+	$updateFile="update.zip";
+	$updateSignatureFile="signature";
+	$updateURL = "https://127.0.0.1/fff/update/index.php";
+	$pubkeyPath = "public_key.pem";
+
+	if(isset($_GET['step'])){
+		$step=$_GET['step'];
+		if($step==="download"){
+		  handleDownload($updateFile,$updateSignatureFile,$pubkeyPath);
+		}
+		if($step==="install"){
+			handleInstall($updateFile,$updateSignatureFile,$triggersFile);
+		}
+	}else{
+		handleDefault($updateURL);
+	}
+
+	function handleDefault($updateURL){
+		$version = getCurrentVersion();
+		echo("<b>Currently installed version: ".$version["version"]." (".$version["buildid"].")</b><br>");
+		$test = doGet($updateURL."?operation=test");
+		if($test[0]!=202){
+			echo("Connection to update server failed. Please check the configured URL<br>");
+		}else{
+			$nv=newVersionAvailable();
+			if($nv!==false){
+				echo("<b>Newer version found :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$nv['version']."(".$nv['buildid'].")</b><br>");
+				echo("<br>".$nv['version']." Release Notes:<br><i>".$nv['releasenotes']."</i><br><br>");
+				echo('<a href="index.php?step=download">Download</a>');
+			}else{
+				echo("<span class=\"oi oi-check\"></span> You are using the newest version.<br><br>Release notes:<br>");
+				echo("<i>".$version['releasenotes']."</i>");
+			}
+		}
+	}
+  
+	function handleInstall($updateFile,$updateSignatureFile,$triggersFile){
+		if(!file_exists($updateFile)||!file_exists($updateSignatureFile)){
+			echo("Missing Update File");
+			echo('<a href="index.php?step=download")>Download</a>');
+		}else{
+			$zip = new ZipArchive;
+			$res = $zip->open($updateFile);
+			if ($res===TRUE) {
+				$path = pathinfo(realpath($updateFile), PATHINFO_DIRNAME);
+				$zip->extractTo($path);
+				$zip->close();
+				echo("Starting Update<br>");
+				if(file_exists($triggersFile)){
+					echo("<span class=\"oi oi-bolt\"></span>Running triggers<br><i>");
+					include($triggersFile);
+					echo("</i><br>");
+				}
+				echo("<span class=\"oi oi-check\"></span> Installation complete.<br>");
+				echo("<br>");
+				?><form action="index.php?p=update" method="POST">
+					<button class="btn" style="background:<?= BRANDING_COLOR ?>">Continue</button>
+				</form> <?php
+			}else{
+				echo("Update failed (1) - ".$res."<br>");
+			}
+			$cu1=unlink($updateFile);
+			$cu2=unlink($updateSignatureFile);
+			$cu3=true;
+			if(file_exists($triggersFile)){
+				$cu3=unlink($triggersFile);
+			}
+			if(!$cu1||!$cu2||!$cu3){
+				echo("Failed to remove update files (".$cu1."-".$cu2."-".$cu3.")<br>");
+			}
+			$updatedVersionFile = updateVersionFile();
+			if(!$updatedVersionFile){
+				echo("Failed to update version file<br>");
+			}
+		}
+	}
+	function handleDownload($updateFile,$updateSignatureFile,$pubkeyPath){
+		$nv=newVersionAvailable();
+		$downloadOK = downloadAndCheckUpdate($updateFile,$updateSignatureFile,$nv['buildid'],$pubkeyPath);
+		if($downloadOK){
+			echo("<span class=\"oi oi-check\"></span> Downloaded newest version<br><span class=\"oi oi-check\"></span> Signature OK<br>");
+			echo('<a href="index.php?step=install">Install</a>');
+		}else{
+			echo("<span class=\"oi oi-x\"></span> Downloaded newest version.<b> Signature INVALID</b>. Aborting");
+		}
+	}
+  
+?>
